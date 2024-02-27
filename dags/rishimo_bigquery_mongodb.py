b7f9dd6a5f395afd397dfd896a2e7ba7e08fca09
rishimo <rishimo2012@gmail.com>
2024-02-27 15:43:11 -0800

    add function and tasks for getting most recent timestamps from mongodb

--- dags/rishimo_bigquery_mongodb.py
+++ dags/rishimo_bigquery_mongodb.py
@@ -1,5 +1,5 @@
 from airflow.models.dag import DAG
-from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
 from airflow.providers.google.cloud.operators.bigquery import (
     BigQueryCreateEmptyDatasetOperator,
     BigQueryDeleteDatasetOperator,
@@ -7,6 +7,9 @@ from airflow.providers.google.cloud.operators.bigquery import (
     BigQueryUpdateDatasetOperator,
 )
 from airflow.providers.mongo.hooks.mongo import MongoHook
+
+import datetime
+import json
 
 # DAG for periodic updates of MongoDB collections from public BigQuery datasets
 # datasets:
@@ -22,9 +25,37 @@ from airflow.providers.mongo.hooks.mongo import MongoHook
 with DAG(
     dag_id="update_mongoDB",
     description="Updates data on MongoDB",
+    start_date=datetime.datetime(2024, 2, 27),
+    schedule="* * * * *",
 ) as dag:
-    
-    def query_mongo(collection: str) -> str:
-        hook = MongoHook(mongo_conn_id='mongo_default')
+
+    def query_mongo(collection_name: str, field_name: str, **context) -> str:
+        hook = MongoHook(mongo_conn_id="mongo_default")
         client = hook.get_conn()
-        
+        db = client.SanFrancisco
+        collection = db[collection_name]
+        print(f"Connected to MongoDB - {client.server_info()}")
+        recent_date_json = collection.find_one(
+            filter={}, projection={field_name: 1, "_id": 0}, sort=[(field_name, -1)]
+        )
+        client.close()
+        date = recent_date_json[field_name]
+
+        # push date to XCOM
+        task_instance = context["task_instance"]
+        task_instance.xcom_push(key=f"latest_{collection_name}_date", value=date)
+        return date
+
+    get_latest_service_date = PythonOperator(
+        task_id="get_latest_service_date",
+        python_callable=query_mongo,
+        op_kwargs={"collection_name": "service_requests", "field_name": "created_date"},
+        dag=dag,
+    )
+
+    get_latest_sffd_date = PythonOperator(
+        task_id="get_latest_sffd_date",
+        python_callable=query_mongo,
+        op_kwargs={"collection_name": "sffd_service_calls", "field_name": "call_date"},
+        dag=dag,
+    )
